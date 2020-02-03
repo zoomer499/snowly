3b61c087ee8abd5bf5c15c9d20403e79a495252d
zoomer499 <zoomer499@gmail.com>
2020-02-03 23:07:02 +0400

    Исправил ошибки в четвертом упражнении

--- ex4.py
+++ ex4.py
@@ -1,11 +1,11 @@
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
+cars = 100 # Колличество автомобилей в парке
+space_in_a_car = 4.0 # Free place in the car.
+drivers = 30 # Count warker
+passengers = 90 # Count new passengers
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_not_driven
+average_passengers_per_car = passengers / cars_driven
 
 
 print ("В наличии", cars, "автомобилей.")
