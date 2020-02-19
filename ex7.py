50935394dbdcad0e74cbf5490db177ffa026eca8
AlVaZuev <zoomer499@gmail.com>
2020-02-19 23:01:11 +0400

    Изменил 6 упражнение поправив ошибки. Перевел на русский 7и 8 упражнения и добавил комментарии к коду.

--- ex7.py
+++ ex7.py
@@ -1,21 +1,45 @@
-print "Mary had a little lamb."
-print "It's fleece was while as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." *10 # what'd that do?
+print ("У Мэри был маленький барашек.") # выводим на экран записть
+print ("Его шерсть была белой как {}.".format('снег')) # выводим на экран записть с добавлением в строку слова
+print("И всюду, куда Мэри шла,") # выводим на экран записть
+print("Маленький барашек всегда следовал за ней.") # выводим на экран записть
+print("."*10) # что это могло значить?
+# Новые переменные:
+end1 = "Б"
+end2 = "а"
+end3 = "д"
+end4 = "д"
+end5 = "и"
+end6 = "Г"
+end7 = "а"
+end8 = "й"
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+# Обратите внимание на запятую в конце строки. Уберите ее. Что произошло?
+print(end1 + end2 + end3 + end4 + end5, end=' ') # суммируем переменные и выводим на экран
+print(end6 + end7 + end8)
+
+
+
+
+
+
+#print "Mary had a little lamb."
+#print "It's fleece was while as %s." % 'snow'
+#print "And everywhere that Mary went."
+#print "." *10 # what'd that do?
+
+#end1 = "C"
+#end2 = "h"
+#end3 = "e"
+#end4 = "e"
+#end5 = "s"
+#end6 = "e"
+#end7 = "B"
+#end8 = "u"
+#end9 = "r"
+#end10 = "g"
+#end11 = "e"
+#end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+#print end1 + end2 + end3 + end4 + end5 + end6,
+#print end7 + end8 + end9 + end10 + end11 + end12
--- ex8.py
+++ ex8.py
@@ -1,13 +1,28 @@
-formatter = "%r %r %r %r"
+formatter = "{} {} {} {}" # вводим новую переменную
 
-print formatter % (1, 2, 3, 4)
-print formatter % ("one", "two", "three", "four")
-print formatter % (True, False, False, True)
-print formatter % (formatter, formatter, formatter, formatter)
-print formatter % (
-	"I had this thing.",
-	"That you could type up right.",
-	"But it didn't sing.",
-	"So I said goodhight."
-)
+print(formatter.format(1,2,3,4)) # добавляем в переменную значения для вывода на экран
+print(formatter.format("раз", "два", "три", "четыре"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+			"Спят в конюшне пони,",
+			"начал пес дремать,",
+			"только мальчик Джонни",
+			"не ложится спать!"
+))
 
+
+
+
+#formatter = "%r %r %r %r"
+
+#print formatter % (1, 2, 3, 4)
+#print formatter % ("one", "two", "three", "four")
+#print formatter % (True, False, False, True)
+#print formatter % (formatter, formatter, formatter, formatter)
+#print formatter % (
+#	"I had this thing.",
+#	"That you could type up right.",
+#	"But it didn't sing.",
+#	"So I said goodhight."
+#)
