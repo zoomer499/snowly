0f3ae5359b57433102f2978ad3df249657c5dd21
AlVaZuev <zoomer499@gmail.com>
2020-02-20 21:40:08 +0400

    Add to ex9 and ex10/ It's new exqmple for me and for git.

--- ex9.py
+++ ex9.py
@@ -1,14 +1,33 @@
+# Несколько непривычный код - набирайте его в точности.
+
+days = "Пн Вт Ср Чт Пт Сб Вс" # переменная дни недели
+months = "\nЯнв\nФевр\nМарт\nАпр\nМай\nИюнь\nИюль\nАвг" # Переменная месяца
+
+print("Это дни недели:", days) # вывести дни недели
+print("А это месяцы:", months) # вывести месяца
+
+print("""
+Что же тут творится?
+Используются три двойные кавычки.
+Мы можем набрать текста сколько угодно.
+Даже 4 строки, 5 или 6.
+""") # добавить в вывод на экран множество строк
+
+
+
+
+
+
 # Here's some new strange stuff, remember type it exactly
 
-days = "Min Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
-print "Here are the days: ", days
-print "Here are the months: ", months
+#print "Here are the days: ", days
+#print "Here are the months: ", months
 
-print """
-There's something going on here.
-With the three double-quotes.
-We'll be able to type as much as we like.
-Even 4 lines if we want, or 5, or 6.
-"""
+#print """
+#There's something going on here.
+#With the three double-quotes.
+#We'll be able to type as much as we like.
+#Even 4 lines if we want, or 5, or 6.
+#"""
