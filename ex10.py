eb6f14c5e11e8e5059380312cfd67d665f6deec9
AlVaZuev <zoomer499@gmail.com>
2020-04-21 16:30:48 +0400

    Обновил два упражнения

--- ex10.py
+++ ex10.py
@@ -1,15 +1,32 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+d_artagnan = "\tМеня зовут д'Артаньян." # переменная с учетом '
+athos = "Эта строка \n разделена на две." # переменая в которой вторая чать начинается с новой строки
+porthos = "Я \\ - \\ мушкетер!"
 
-fat_cat = """
-I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
-"""
+aramis = """
+Подсказки:
+\t* Граф из Гаскона
+\t* Связан с Миледи
+\t* Барон \n\t* Генерал Ордена
+""" # переменая в которой используется три кавычки для большого абзаца
 
-print tabby_cat
-print persian_cat
-print backslash_cat
-print fat_cat
+print(d_artagnan) # дальше выводятся на экран все переменные поочередно
+print(athos)
+print(porthos)
+print(aramis)
+
+
+#tabby_cat = "\tI'm tabbed in."
+#persian_cat = "I'm split\non a line."
+#backslash_cat = "I'm \\ a \\ cat."
+
+#fat_cat = """
+#I'll do a list:
+#\t* Cat food
+#\t* Fishies
+#\t* Catnip\n\t* Grass
+#"""
+
+#print tabby_cat
+#print persian_cat
+#print backslash_cat
+#print fat_cat
