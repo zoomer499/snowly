a0919e6d7bc691d717efb2bc0b9ae786dcfb50d7
AlVaZuev <zoomer499@gmail.com>
2020-05-13 08:30:25 +0400

    Ex.11, don't test

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("Сколько тебе лет?", end=' ')
+age = input()
+print("Каков твой рост?", end=' ')
+height = input()
+print("Каков твой рост?"? end=' ')
+weight = input()
+
+print(f"Итак, тебе {age} лет, в тебе {height} см роста и {weight} кг веса.")
