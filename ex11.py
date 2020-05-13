a6b2a96f3df0457b25561901cf91dd7e78ddcd0c
AlVaZuev <zoomer499@gmail.com>
2020-05-13 08:39:05 +0400

    Исправил ошибки синтаксиса

--- ex11.py
+++ ex11.py
@@ -1,8 +1,8 @@
-print("Сколько тебе лет?", end=' ')
+print("Сколько тебе лет?", end =' ')
 age = input()
-print("Каков твой рост?", end=' ')
+print("Каков твой рост?", end =' ')
 height = input()
-print("Каков твой рост?"? end=' ')
+print("Каков твой вес?", end =' ')
 weight = input()
 
 print(f"Итак, тебе {age} лет, в тебе {height} см роста и {weight} кг веса.")
